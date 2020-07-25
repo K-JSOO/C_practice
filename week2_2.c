060dab4c31f02468a32a6b69af537ba02cc972a3
K-JSOO <59533941+K-JSOO@users.noreply.github.com>
2020-07-25 16:46:57 +0900

    Update week2_2.c

--- week2_2.c
+++ week2_2.c
@@ -5,5 +5,5 @@ int main(void)
 {
     float money = get_float("How many save your money?\n");
     float re_money = money * 1.012;
-    printf("After 1 year, your money is %.2f",re_money);
+    printf("After 1 year, your money is %.2f\",re_money);
 }
